84f0f5eea99c930b49d772ed88ff47b67fbb60d5
huoli-knight <158906158@qq.com>
2019-07-16 21:55:43 +0800

    系统信息常数项部分功能实现

---
+++ src/com/localhost/model/mapper/ConstantMapper.java
@@ -0,0 +1,11 @@
+package com.localhost.model.mapper;
+
+
+import java.util.List;
+
+import com.localhost.model.po.Constant;
+
+public interface ConstantMapper {
+
+	public List<Constant> getConstantData();
+}
